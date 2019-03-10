ffc4c52e34320b756cad9aff7e5041035f73f757
Kaitian Xie <xkaitian@gmail.com>
2019-03-09 20:06:34 -0800

    Implement getJson

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const endpoints = require('./endponts');
+const { values } = require('./default');
+const axios = require('axios');
+const cache = require('memory-cache');
 
 class Zomato {
   constructor(config) {
@@ -10,5 +13,58 @@ class Zomato {
     });
   }
 }
+
+const getJson = (config, url, opts, cb) => {
+  const cachedResult = cache.get(url);
+  if (cachedResult !== null) {
+    return new Promise((resolve, reject) => {
+      if (cb) {
+        cb(cachedResult, false);
+      }
+      resolve(cachedResult);
+    });
+  } else {
+    const zomatoApi = axios.create({
+      baseUrl: `${values.protocol}${values.hostName}`,
+      timeout: values.timeout,
+      headers: { 'user-key': config.userKey }
+    });
+
+    return zomatoApi
+      .get(url, opts)
+      .then(response => {
+        if (response) {
+          if (
+            response.statusCode !== undefined &&
+            response.statusCode !== 200
+          ) {
+            if (!cb) {
+              throw response;
+            } else {
+              cb('error', response);
+            }
+          } else {
+            response = response.data;
+            if (values.cacheLimit > 0) {
+              cache.put(url, response, value.cacheLimit);
+            }
+
+            if (cb) {
+              cb(response, false);
+            } else {
+              return response;
+            }
+          }
+        }
+      })
+      .catch(error => {
+        if (!cb) {
+          throw error;
+        } else {
+          cb('error', error);
+        }
+      });
+  }
+};
 
 module.exports = Zomato;
